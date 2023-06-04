2c9b8580923849ca9f15fd02a8c296e882ee41ce
Teboho Nthako <tjaynthako@gmail.com>
2023-06-04 23:49:57 +0200

    functionality added with onclick attributes to buttons

--- script.js
+++ script.js
@@ -18,13 +18,9 @@
 
 
 // Number Buttons or operands and operators functionality
-
-const clear = document.getElementById("clear").value;
-const percent = document.getElementById("percent").value;
-const negative = document.getElementById("negative").value;
-const num9 = document.getElementById("9").value;
-const num8 = document.getElementById("8").value;
-const num7 = document.getElementById("7").value;
+const num9 = document.getElementById("9");
+const num8 = document.getElementById("8");
+const num7 = document.getElementById("7");
 const num6 = document.getElementById("6");
 const num5 = document.getElementById("5");
 const num4 = document.getElementById("4");
@@ -38,88 +34,94 @@ const numSubtract = document.getElementById("subtract");
 const numMultiply = document.getElementById("multiply");
 const numDivide = document.getElementById("divide");
 const numAnswer = document.getElementById("answer");
+const clear = document.getElementById("clear");
+const backspace = document.getElementById("backspace");
+const percent = document.getElementById("negative");
 
 clear.addEventListener("click", () => {
-  document.getElementById("finalanswer").innerHTML = "";
-});
+  document.getElementById("display-box").innerHTML = "";
+})
 
 percent.addEventListener("click", () => {
-  document.getElementById("finalanswer").innerHTML += "%";
-});
-negative.addEventListener("click", () => {
-  document.getElementById("finalanswer").innerHTML += "-/+";
-});
+  document.getElementById("display-box").innerHTML += "-";
+})
 
-num9.addEventListener("click", () => {
-  document.getElementById("finalanswer").innerHTML += "9";
-});
+num9.addEventListener("click", () => {  
+  document.getElementById("display-box").innerHTML += "9";
+})
 
-num8.addEventListener("click", () => {
-  document.getElementById("finalanswer").innerHTML += "8";
+
+num8.addEventListener("click", () => {  
+  document.getElementById("display-box").innerHTML += "8";
+})
+
+
+num7.addEventListener("click", () => {  
+  document.getElementById("display-box").innerHTML += "7";
   console.log("9");
-});
+})
 
-num7.addEventListener("click", () => {
-  document.getElementById("finalanswer").innerHTML += "7";
+
+num6.addEventListener("click", () => {  
+  document.getElementById("display-box").innerHTML += "6";
   console.log("9");
-});
+})
 
-num6.addEventListener("click", () => {
-  document.getElementById("finalanswer").innerHTML += "6";
+
+num5.addEventListener("click", () => {  
+  document.getElementById("display-box").innerHTML += "5";
   console.log("9");
-});
+})
 
-num5.addEventListener("click", () => {
-  document.getElementById("finalanswer").innerHTML += "5";
+
+num4.addEventListener("click", () => {  
+  document.getElementById("display-box").innerHTML += "4";
   console.log("9");
-});
+})
 
-num4.addEventListener("click", () => {
-  document.getElementById("finalanswer").innerHTML += "4";
+
+num3.addEventListener("click", () => {  
+  document.getElementById("display-box").innerHTML += "3";
   console.log("9");
-});
+})
 
-num3.addEventListener("click", () => {
-  document.getElementById("finalanswer").innerHTML += "3";
+num2.addEventListener("click", () => {  
+  document.getElementById("display-box").innerHTML += "2";
   console.log("9");
-});
+})
 
-num2.addEventListener("click", () => {
-  document.getElementById("finalanswer").innerHTML += "2";
+num1.addEventListener("click", () => {  
+  document.getElementById("display-box").innerHTML += "1";
   console.log("9");
-});
+})
 
-num1.addEventListener("click", () => {
-  document.getElementById("finalanswer").innerHTML += "1";
+num0.addEventListener("click", () => {  
+  document.getElementById("display-box").innerHTML += "0";
   console.log("9");
-});
+})
 
-num0.addEventListener("click", () => {
-  document.getElementById("output").innerHTML += "0";
-  console.log("9");
-});
 
-numdot.addEventListener("click", () => {
-  document.getElementById("output").innerHTML += ".";
-});
+numdot.addEventListener("click", () => {  
+  document.getElementById("display-box").innerHTML += ".";
+})
 
-numAdd.addEventListener("click", () => {
-  document.getElementById("output").innerHTML += "+";
-});
+numAdd.addEventListener("click", () => {  
+  document.getElementById("display-box").innerHTML += "+";
+})
 
-numSubtract.addEventListener("click", () => {
-  document.getElementById("output").innerHTML += "-";
-});
+numSubtract.addEventListener("click", () => {  
+  document.getElementById("display-box").innerHTML += "-";
+})
 
-numDivide.addEventListener("click", () => {
-  document.getElementById("output").innerHTML += "/";
-});
+numDivide.addEventListener("click", () => {  
+  document.getElementById("display-box").innerHTML += "/";
+})
 
-numMultiply.addEventListener("click", () => {
-  document.getElementById("output").innerHTML += "*";
-});
+numMultiply.addEventListener("click", () => {  
+  document.getElementById("display-box").innerHTML += "*";
+})
 
 numAnswer.addEventListener("click", () => {
-  const num = document.getElementById("output").textContent;
-  document.getElementById("").innerHTML = Function(`return ${num}`)();
-});
+  const num = document.getElementById("display-box").textContent;
+  document.getElementById("test").innerHTML = Function(`return ${num}`)();
+})
